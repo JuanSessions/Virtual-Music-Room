e071975f56b4857cb23408eceb6b5754a50ed7fd
JuanSessions <juanperusin@yahoo.es>
2020-07-19 17:28:24 +0200

    build conflict fixed?

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a305795af786e1a32942a142fe634bd6.js"
+  "./precache-manifest.091458b18611834263fe96dffa2e3f3c.js"
 );
 
 self.addEventListener('message', (event) => {
