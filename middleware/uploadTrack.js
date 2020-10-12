8d93e57aaaf8d3960bcf7ccb58c6a8e95cafd92d
JuanSessions <juanperusin@yahoo.es>
2020-10-12 13:45:49 +0200

    projects uploads working again

--- middleware/uploadTrack.js
+++ middleware/uploadTrack.js
@@ -1,5 +1,6 @@
 const multer = require("multer")
 const uploadPath = `${__dirname}/../client/build/uploads/tracks`;
+console.log(`${__dirname}/../client/build/uploads/tracks`)
 
 const upload = multer({
     dest: uploadPath
