bf16851e751923c86ffc6cedb9fc01978afb89aa
JuanSessions <juanperusin@yahoo.es>
2020-10-12 15:47:29 +0200

    info bubble n check auto-deploy

--- client/src/components/InfoHome.jsx
+++ client/src/components/InfoHome.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function InfoHome() {
-    return (
-        <div className="speech-bubble">
-            <h2>Current Info</h2>
-            <p>Our chat room is in progress.</p>
-            <p>Sign Up easily and for free to check out audio players, uploads, etc.</p>
-        </div>
-    )
+  return (
+    <div className="speech-bubble">
+      <h2>Current Info</h2>
+      <p>Our chat room is in progress.</p>
+      <p>
+        Sign Up easily and for free to check out our audio players, upload a
+        track, start a project, etc.
+      </p>
+    </div>
+  );
 }
 
-export default InfoHome
-
-
+export default InfoHome;
