4eddb46acae2451309b9b7a96450bb7f8bbea693
JuanSessions <juanperusin@yahoo.es>
2020-10-14 15:29:02 +0200

    build file problem

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f8e4c7a3c014da466263e678f391c05.js"
+  "/precache-manifest.a70e58b0c7229050408b0d229f0ebdf7.js"
 );
 
 self.addEventListener('message', (event) => {
