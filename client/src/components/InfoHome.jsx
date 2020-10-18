0f8e7ab41c52f7b89977d8b4939e4cd88a7eb553
JuanSessions <juanperusin@yahoo.es>
2020-10-18 15:39:40 +0200

    more text bubble

--- client/src/components/InfoHome.jsx
+++ client/src/components/InfoHome.jsx
@@ -7,7 +7,7 @@ function InfoHome() {
       <p>Our chat room is in progress.</p>
       <p>
         Sign Up easily and for free to check out our audio players, upload a
-        track, start a project, ask questions, etc. 
+        track, start a project, ask questions, etc. It help us in the production process.
       </p>
     </div>
   );
